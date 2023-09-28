cb5537faf5ee52ee52d505dabc4469b22dd7929d
zyelda <146381037+zyelda@users.noreply.github.com>
2023-09-29 04:23:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Langkah 1: Tambahkan API key Anda
+const apiKey = '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';
+
+// Langkah 2: Fungsi untuk Mengirim Permintaan ke ChatGPT
+async function getChatGPTResponse(query) {
+    // ...
+}
+
+// Langkah 3: Fungsi untuk Menampilkan Hasil pada Website Anda
+function displayResults(response) {
+    // ...
+}
+
+// Langkah 4: Mendengarkan Tombol Cari dan Menanggapi Input Pengguna
+document.getElementById('search-button').addEventListener('click', async function() {
+    // ...
+});
