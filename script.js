b89c4509970e0e8bf6835724f22dfc9abafcd8c0
zyelda <146381037+zyelda@users.noreply.github.com>
2023-09-30 15:50:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-// Langkah 1: Tambahkan API key Anda
-const apiKey = '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';
-
-// Langkah 2: Fungsi untuk Mengirim Permintaan ke ChatGPT
-async function getChatGPTResponse(query) {
-    // ...
+function submitQuestion() {
+    var inputQuestion = document.getElementById("inputQuestion").value;
+    var responseContainer = document.getElementById("responseContainer");
+    fetch('https://api.openai.com/v1/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 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'
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            prompt: inputQuestion,
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Pastikan data.choices adalah array dan memiliki setidaknya satu elemen
+        if (Array.isArray(data.choices) && data.choices.length > 0) {
+            var response = data.choices[0].text.trim();
+            responseContainer.innerHTML += "<p>" + response + "</p>";
+            document.getElementById("inputQuestion").value = "";
+        } else {
+            console.error('Error: Unexpected response format');
+        }
+    })
+    .catch(error => console.error('Error:', error));
 }
-
-// Langkah 3: Fungsi untuk Menampilkan Hasil pada Website Anda
-function displayResults(response) {
-    // ...
-}
-
-// Langkah 4: Mendengarkan Tombol Cari dan Menanggapi Input Pengguna
-document.getElementById('search-button').addEventListener('click', async function() {
-    // ...
-});
